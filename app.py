b7eb6687bb7f99005ec35a8b5b9edc2f41c3b3e4
Valentyn Deshel <153648477+glancyF@users.noreply.github.com>
2025-05-14 18:19:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ with open('assets/styles.css', encoding='utf-8') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 st.title("FOOTBALL DASHBOARD 🚀")
-st.write("Welcome to the admin panel")
 
 menu = ["Main", "Load matches", "View matches"]
 choice = st.sidebar.selectbox("Menu", menu)
